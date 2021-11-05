bc9ffd7502ad276ae00f746a47e8f9ae37631a3e
Sina Nazari <sinanazari3000@gmail.com>
2021-11-05 11:40:29 +0330

    Create Or Update Source Code

---
+++ OOP-Example_3/OOP-Example_3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace OOP_Example_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public void ReadFile()
+        {
+            string filePath = @"C:\To\Test.txt";
+            FileStream file = null;
+            try
+            {
+                file = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+                var fileSize = file.ReadByte();
+            }
+            finally
+            {
+                file.Close();
+            }
+        }
+    }
+}
