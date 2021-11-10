105b34667244d690b93d9b7919d643c47a669ebf
Sina Nazari <sinanazari3000@gmail.com>
2021-11-10 17:14:27 +0330

    Update Or Create Source Code

--- OOP-Example_7/OOP-Example_7/Program.cs
+++ OOP-Example_7/OOP-Example_7/Program.cs
@@ -8,31 +8,71 @@ class Program
         //car.model = "98";
 
         var car = new Car();
-        var car2 = new Car("Pikan", 200, "80");
+        car.name = "pikan";
+        car.speed = 115;
+        car.model = 91;
+        //var car2 = new Car("Pikan", 200, "80");
         Console.WriteLine($"Name:{car.name} Speed:{car.speed} Model:{car.model}");
-        Console.WriteLine($"Name:{car2.name} Speed:{car2.speed} Model:{car2.model}");
+        //Console.WriteLine($"Name:{car2.name} Speed:{car2.speed} Model:{car2.model}");
     }
 }
 
 public class Car
 {
-    public int speed;
-    public string name;
-    public string model;
+    //public int speed;
+    //public string name;
+    //public string model;
 
-    public Car()
+    private string _name;
+    private int _speed;
+    private int _model;
+
+    public string name { get; set; } = "Pridy";
+    public int speed
     {
-        this.name = "Object";
-        this.speed = 120;
-        this.model = "90";
+        get { return _speed; }
+        set
+        {
+            if (value >= 120)
+            {
+                throw new Exception("Value Speed Is Not Valid...");
+            }
+            else
+            {
+                _speed = value;
+            }
+        }
+    }
+    public int model
+    {
+        get { return _model; }
+        set
+        {
+            if (value < 90)
+            {
+                throw new Exception("This Cars is Destructor...");
+            }
+            else
+            {
+                _model = value;
+            }
+        }
     }
 
-    public Car(string name, int speed, string model)
-    {
-        this.name = name;
-        this.speed = speed;
-        this.model = model;
-    }
+
+    //public Car()
+    //{
+    //    this.name = "Object";
+    //    this.speed = 120;
+    //    this.model = "90";
+    //}
+
+    //public Car(string name, int speed, string model)
+    //{
+    //    this.name = name;
+    //    this.speed = speed;
+    //    this.model = model;
+    //}
     //~Car()
     //{
     //    Console.WriteLine("Destructor");
