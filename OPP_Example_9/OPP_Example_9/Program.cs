29d06ad572075e83c86fd8b8b636e81df561d0d6
Sina Nazari <sinanazari3000@gmail.com>
2021-11-12 09:52:51 +0330

    Update Or Create Source Code

---
+++ OPP_Example_9/OPP_Example_9/Program.cs
@@ -0,0 +1,35 @@
+﻿
+//Pass By Reference Parameters
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MyClass my = new MyClass();
+        int a = 8;
+        int b = 3;
+        int sum = my.Sum(ref a, ref b);
+
+        int x;
+        my.My(out x);
+    }
+}
+
+class MyClass
+{
+    public int Sum(ref int a, ref int b)
+    {
+        a = 9;
+        b = 9;
+        return a + b;
+    }
+
+    //Pass By Output Parameters
+    public void My(out int x)
+    {
+        x = 20;
+    }
+}
+
+
+//
