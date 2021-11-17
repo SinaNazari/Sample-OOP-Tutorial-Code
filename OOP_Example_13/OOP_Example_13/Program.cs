75c94c41829b1e22f3fde4713b9384fae190ae19
Sina Nazari <sinanazari3000@gmail.com>
2021-11-17 20:52:12 +0330

    Update Or Create Source Code

---
+++ OOP_Example_13/OOP_Example_13/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+    }
+}
+
+class Person
+{
+    private int id;
+    protected string? phone;
+    public string? Name;
+
+    public void Send()
+    {
+        phone = "098655474";
+    }
+}
+
+class Employees : Person
+{
+    public string GetPhone()
+    {
+        return phone;
+    }
+}
