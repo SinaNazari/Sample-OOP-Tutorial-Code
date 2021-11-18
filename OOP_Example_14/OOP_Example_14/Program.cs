d0ae5e5249e7ce740073c02596c76b939b2af7cb
Sina Nazari <sinanazari3000@gmail.com>
2021-11-19 01:43:15 +0330

    Update Or Create Source Code

---
+++ OOP_Example_14/OOP_Example_14/Program.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Employees e=new Employees();
+        e.name = "Sina";
+        e.family = "Nazari";
+        e.SayHello();
+    }
+}
+
+class Person
+{
+    public string name;
+
+    public void SayHello()
+    {
+        Console.WriteLine("Hello" + " " + name);
+    }
+}
+
+class Employees : Person
+{
+    public string family;
+
+    public new void SayHello()
+    {
+        Console.WriteLine("Hello"+" "+name+" "+family);
+        base.SayHello();
+    }
+}
