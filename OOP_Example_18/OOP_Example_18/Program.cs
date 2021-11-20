317c724c02f889e9abcb9bc4a4861ad293876681
Sina Nazari <sinanazari3000@gmail.com>
2021-11-21 02:24:57 +0330

    Update Or Create Source Code

---
+++ OOP_Example_18/OOP_Example_18/Program.cs
@@ -0,0 +1,34 @@
+﻿public sealed class Program
+{
+    static void Main(string[] args)
+    {
+        //Parent ch =new Parent();
+        //ChildA ch = new ChildA();
+        ChildB ch = new ChildB();
+        ch.SayHello();
+    }
+}
+
+public class Parent
+{
+    public virtual void SayHello()
+    {
+        Console.WriteLine("Hello..");
+    }
+}
+
+public class ChildA : Parent
+{
+    public override void SayHello()
+    {
+        Console.WriteLine("Hi...");
+    }
+}
+
+public class ChildB : ChildA
+{
+    public override void SayHello()
+    {
+        Console.WriteLine("Hello >>>>>>");
+    }
+}
