6255f0a9f63449f71d34ef9c91b0dc701ae23d82
Sina Nazari <sinanazari3000@gmail.com>
2021-11-21 03:22:19 +0330

    Update Or Create Source Code

---
+++ OOP_Example_20/OOP_Example_20/Program.cs
@@ -0,0 +1,50 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Person p = new Person();
+        p.Age = 22;
+        CheckAge(p);
+
+        Person p2 = new Employees();
+        p2.Age = 12;
+        CheckAge(p2);
+
+        Person p3 = new Faculty();
+        p3.Age = 33;
+        CheckAge(p3);
+
+        void CheckAge(Person person)
+        {
+            if (person is Employees)
+            {
+                Console.WriteLine("Employees");
+            }
+            else if (person is Faculty)
+            {
+                Console.WriteLine("Faculty");
+            }
+            else
+            {
+                Console.WriteLine("Person");
+            }
+        }
+    }
+}
+
+class Person
+{
+    public string Name { get; set; }
+    public string Family { get; set; }
+    public int Age { get; set; }
+}
+
+class Employees : Person
+{
+    public double Salary { get; set; }
+}
+
+class Faculty : Person
+{
+    public string Name { get; set; }
+}
