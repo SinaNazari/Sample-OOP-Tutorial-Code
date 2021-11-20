40230c88921bab43db757ae1828f6c6f40e86a4c
Sina Nazari <sinanazari3000@gmail.com>
2021-11-20 05:54:02 +0330

    Update Or Create Source Code

---
+++ OOP_Example_16/OOP_Example_16/Program.cs
@@ -0,0 +1,30 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Child ch = new Child();
+        ch.Id = 1;
+        ch.Name = "Sina";
+        ch.SayHello();
+    }
+}
+
+abstract class Parent
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public abstract void SayHello();
+    public void SayHi()
+    {
+        Console.WriteLine("Hi...");
+    }
+}
+
+class Child : Parent
+{
+    public override void SayHello()
+    {
+        Console.WriteLine("Hello User...");
+    }
+}
