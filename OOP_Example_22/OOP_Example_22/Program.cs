73c8be6a2e0ea23703cd0b3cfcc7d7ff5e6ad7d7
Sina Nazari <sinanazari3000@gmail.com>
2021-11-21 19:16:56 +0330

    Update Or Create Source Code

---
+++ OOP_Example_22/OOP_Example_22/Program.cs
@@ -0,0 +1,25 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        MyDelegate myDelegate = new MyDelegate(SayHello);
+        myDelegate("Hello");
+
+    }
+
+    public delegate void MyDelegate(string message);
+    public static void SayHello(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
+//    public int sum(int a, int b)
+//    {
+//        return a + b;
+//    }
+
+//    public string FullName(string name, string family)
+//    {
+//        return name + " " + family;
+//    }
+
