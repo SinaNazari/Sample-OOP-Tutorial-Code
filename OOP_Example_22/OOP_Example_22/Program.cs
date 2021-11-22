5c6908ce339a2a8537227b266d6fb19b8b76580d
Sina Nazari <sinanazari3000@gmail.com>
2021-11-22 12:32:10 +0330

    Update Or Create Source  Code

--- OOP_Example_22/OOP_Example_22/Program.cs
+++ OOP_Example_22/OOP_Example_22/Program.cs
@@ -3,23 +3,32 @@
     static void Main(string[] args)
     {
         MyDelegate myDelegate = new MyDelegate(SayHello);
-        myDelegate("Hello");
+        myDelegate += SayHello2;
+        myDelegate += SayHello3;
+        myDelegate += SayHello4;
+        myDelegate("Sina Nazari");
 
     }
 
     public delegate void MyDelegate(string message);
-    public static void SayHello(string message)
+    public static void SayHello(string name)
     {
-        Console.WriteLine(message);
+        Console.WriteLine("hi"+" "+name);
+    }
+
+    public static void SayHello2(string name)
+    {
+        Console.WriteLine("How Is It Going?" + " " + name);
+    }
+
+    public static void SayHello3(string name)
+    {
+        Console.WriteLine("How Old Are You?" + " " + name);
+    }
+
+    public static void SayHello4(string name)
+    {
+        Console.WriteLine("Whats You Favorite Color?" + " " + name);
     }
 }
-//    public int sum(int a, int b)
-//    {
-//        return a + b;
-//    }
-
-//    public string FullName(string name, string family)
-//    {
-//        return name + " " + family;
-//    }
 
